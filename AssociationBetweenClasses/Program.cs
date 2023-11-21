9b03aec9413f10ee8cb416cae5af458657caa5ec
Emmanuel Ochoa <eorivera95@gmail.com>
2023-11-20 19:27:26 -0600

    Add project files.

---
+++ AssociationBetweenClasses/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AssociationBetweenClasses
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			var text = new Text();
+			text.Copy();
+
+			text.Width = 100;
+			text.Copy();
+        }
+	}
+}
