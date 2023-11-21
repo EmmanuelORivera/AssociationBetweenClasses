e2dfb0b2335cf06fcb15d8e2b60a81131be53439
Emmanuel Ochoa <eorivera95@gmail.com>
2023-11-20 21:10:46 -0600

    feat: composition and inheritance examples

--- AssociationBetweenClasses/Program.cs
+++ AssociationBetweenClasses/Program.cs
@@ -6,11 +6,28 @@ namespace AssociationBetweenClasses
 	{
 		static void Main(string[] args)
 		{
+			UseInheritance();
+			UseComposition();
+			
+		}
+
+		static void UseInheritance()
+		{
 			var text = new Text();
 			text.Copy();
 
 			text.Width = 100;
 			text.Copy();
-        }
+		}
+		static void UseComposition()
+		{
+			var dbMigrator = new DbMigrator(new Logger());
+
+			var logger = new Logger();
+			var installer = new Installer(logger));
+
+			dbMigrator.Migrate();
+			installer.Install();
+		}
 	}
 }
