19a40bf7cc2d07c3fafb02965522e54840ea8efe
Emmanuel Ochoa <eorivera95@gmail.com>
2023-11-21 18:25:06 -0600

    feat: Desing a Stack

--- AssociationBetweenClasses/Program.cs
+++ AssociationBetweenClasses/Program.cs
@@ -24,16 +24,42 @@ namespace AssociationBetweenClasses
 			title.Width = 200;
 			shape.Width = 100;
 
-            Console.WriteLine(title.Width);
+			Console.WriteLine(title.Width);
 
 			StreamReader reader = new StreamReader(new MemoryStream()); // the new MemoryStream is upcasting to the Strem class
+			UseStack();
 		}
 
+		static void UseStack()
+		{
+			var stack = new Stack();
+			stack.Push(1);
+			stack.Push(2);
+			//stack.Push(null);
+			//stack.Clear();
+            Console.WriteLine(stack.Pop());
+			Console.WriteLine(stack.Pop());
+			Console.WriteLine(stack.Pop());
+		}
 		static void UseBoxingAndUnboxing()
 		{
+
+			// there are 2 types of variables in C#
+			// value types: Numerical, Floating numbers, Char, Boolean
+			// reference types: object, string
 			var list = new ArrayList();
+			//BOXING num to object
 			list.Add(1);
 			list.Add("Emmanuel");
+
+			var thisObj = list[0]; // this is not a number
+			var number = (int)list[0]; // this is casted to int
+
+			var anotherList = new List<int>();
+			anotherList.Add(1);
+			var test = anotherList[0]; // this is an integer
+			var names = new List<string>();
+			names.Add("a");
 		}
 		static void UseConstructorAndInheritance()
 		{
---
+++ AssociationBetweenClasses/Stack.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+
+namespace AssociationBetweenClasses
+{
+	// FIFO EXAMPLE
+	public class Stack
+	{
+		private readonly ArrayList _stack = new();
+		public void Push(object obj)
+		{
+			if (obj == null)
+			{
+				throw new InvalidOperationException("Null is not a valid value");
+			}
+			_stack.Add(obj);
+
+		}
+		public object Pop()
+		{
+			if (_stack.Count <= 0)
+			{
+				throw new InvalidOperationException("Can't pop value from an empty stack");
+			}
+            var temp = (int)_stack[_stack.Count - 1]!;
+			_stack.RemoveAt(_stack.Count - 1);
+
+			object obj = temp;
+            return obj;
+		}
+		public void Clear()
+		{
+			_stack.Clear();
+		}
+	}
+}
